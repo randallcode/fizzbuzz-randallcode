44dc6de134865b38c82fe2afe60e93897d7220e3
Randy Egan <121986322+randallcode@users.noreply.github.comclear>
2023-07-23 17:12:04 -0400

    Added code to fizzbuzz.py file

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
-# add your code here
-
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    if i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    #elif i % 3 == 0 and i % 5 == 0:
+    #   print("FizzBuzz")
+    else:
+        print(i)# add your code here
